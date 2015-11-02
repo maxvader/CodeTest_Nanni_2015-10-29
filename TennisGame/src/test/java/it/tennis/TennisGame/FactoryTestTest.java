cdd85d6a00f2221b76762584bad2e7db4d36ecf1
Massimo Nanni <m.nanni@cineca.it>
2015-11-03 00:28:22 +0100

    New game shoud not be over

---
+++ TennisGame/src/test/java/it/tennis/TennisGame/FactoryTestTest.java
@@ -0,0 +1,36 @@
+package it.tennis.TennisGame;
+
+import it.tennis.TennisGame.interfaces.AbstractGame;
+import it.tennis.TennisGame.simpleImpl.SimpleFactory;
+
+import javax.inject.Inject;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/test-context.xml" })
+public class FactoryTestTest {
+	@Inject
+	SimpleFactory simpleFactory;
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testNewGameNotOver() {
+		AbstractGame ag = simpleFactory.createGame();
+		Assert.assertTrue(ag.isGameRunning());
+	}
+
+}
